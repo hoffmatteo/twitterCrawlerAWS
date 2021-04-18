30d2079e27a0e2b9c430709919a95d305ffeb44b
Matteo Hoffmann <hoffmatteo@gmail.com>
2021-04-18 20:31:32 +0200

    Created React.js project, first tries at React

---
+++ react_frontend/src/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+/*ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+*/
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+
+  }
+}
+
+
+function tick() {
+  ReactDOM.render( <
+    Clock
+    />,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
